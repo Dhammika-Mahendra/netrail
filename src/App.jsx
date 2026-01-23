f969386e094e516d36429ea7b9ff087ca89a0fe8
dhammikamahendra <wmdhammikamahendra@gmail.com>
2026-01-23 18:36:57 +0530

    basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import './index.css'
 import { AppProvider } from './context/AppContext'
+import Nav from './components/Nav'
+import Map from './components/Map'
 
 
 function App() {
 
   return (
     <AppProvider>
-      <div className="relative flex">
-       <h1 className="absolute top-4 left-4 text-2xl font-bold">NetRail</h1>
+      <div className="relative flex w-full h-screen">
+       <Nav></Nav>
+       <Map></Map>
       </div>
     </AppProvider>
   )
---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div className='w-[20%] h-full bg-white shadow-md'>
+      <h4>Nav bar</h4>
+    </div>
+  )
+}
