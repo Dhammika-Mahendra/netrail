8b1a6122271749fcf7bf0554c0ce0193bde8bed5
dhammikamahendra <wmdhammikamahendra@gmail.com>
2026-01-23 19:11:25 +0530

    Path layer test render on base map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import pathDataUrl from '../assets/path.geojson?url'
 
 export default function Map() {
+  const [pathData, setPathData] = useState(null)
+  
+  // Sri Lanka center coordinates
+  const center = [7.8731, 80.7718]
+  const zoom = 8
+
+  useEffect(() => {
+    fetch(pathDataUrl)
+      .then(response => response.json())
+      .then(data => setPathData(data))
+  }, [])
+
   return (
-    <div>
-      This is map
+    <div className='h-full w-full'>
+      <MapContainer
+        center={center}
+        zoom={zoom}
+        className='h-full w-full'
+        scrollWheelZoom={true}
+        zoomControl={false}
+        attributionControl={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {pathData && (
+          <GeoJSON 
+            data={pathData}
+            style={{
+              color: '#3388ff',
+              weight: 2,
+              opacity: 0.8
+            }}
+          />
+        )}
+      </MapContainer>
     </div>
   )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Nav() {
   return (
-    <div className='w-[20%] h-full bg-white shadow-md'>
+    <div className='w-[20%] h-full bg-white shadow-lg shadow-black-500/50'>
       <h4>Nav bar</h4>
     </div>
   )
